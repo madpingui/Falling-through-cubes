3e3f679f041ded990243ca81d0d2e0fecf191343
Juan Manuel Rúa Trujillo <juanm.ruat@gmail.com>
2025-07-03 00:25:49 +0400

    Update API

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,8 +31,8 @@ public class Player : MonoBehaviour
 
         Time.timeScale += Time.fixedDeltaTime * 0.01f; 
         transform.rotation *= Quaternion.Euler(0, 0, Time.deltaTime * 7f);
-        rb.velocity += transform.rotation * ((Input.acceleration.x - xatstart) * Vector3.right * 15f) * Time.deltaTime;
-        rb.velocity += transform.rotation * ((Input.acceleration.y - yatstart) * Vector3.up * 15f) * Time.deltaTime;
+        rb.linearVelocity += transform.rotation * ((Input.acceleration.x - xatstart) * Vector3.right * 15f) * Time.deltaTime;
+        rb.linearVelocity += transform.rotation * ((Input.acceleration.y - yatstart) * Vector3.up * 15f) * Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
